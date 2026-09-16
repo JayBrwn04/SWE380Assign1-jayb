0a53b11540d9e9d560fea5211a3d20abc7893268
JayBrwn04 <143734151+JayBrwn04@users.noreply.github.com>
2026-09-16 19:43:33 -0400

    commented out unused parts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-
+/*
 // element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
@@ -13,10 +13,10 @@ const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
 // sidebar toggle functionality for mobile
 sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
+*/
 
-
-
-/*// testimonials variables
+/*
+// testimonials variables
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
@@ -31,7 +31,7 @@ const modalText = document.querySelector("[data-modal-text]");
 const testimonialsModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
-}*/
+}
 
 // add click event to all modal items
 for (let i = 0; i < testimonialsItem.length; i++) {
@@ -52,9 +52,9 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 // add click event to modal close button
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
+*/
 
-
-
+/*
 // custom select variables
 const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
@@ -112,9 +112,9 @@ for (let i = 0; i < filterBtn.length; i++) {
   });
 
 }
+*/
 
-
-
+/*
 // contact form variables
 const form = document.querySelector("[data-form]");
 const formInputs = document.querySelectorAll("[data-form-input]");
@@ -133,7 +133,7 @@ for (let i = 0; i < formInputs.length; i++) {
 
   });
 }
-
+*/
 
 
 // page navigation variables
